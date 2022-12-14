aeac2dac0416d5a2930cc3b7ef52b05c6b1323fc
User <User@erg1-pc13>
2022-12-14 16:20:28 +0200

    Prwti allagi tou project.

---
+++ src/main/java/com/mycompany/championchip/Player.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.championchip;
+
+/**
+ *
+ * @author User
+ */
+public class Player {
+    
+    private String FirstName;
+
+    /**
+     * Get the value of FirstName
+     *
+     * @return the value of FirstName
+     */
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    /**
+     * Set the value of FirstName
+     *
+     * @param FirstName new value of FirstName
+     */
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    private String LastName;
+
+    /**
+     * Get the value of LastName
+     *
+     * @return the value of LastName
+     */
+    public String getLastName() {
+        return LastName;
+    }
+
+    /**
+     * Set the value of LastName
+     *
+     * @param LastName new value of LastName
+     */
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    private String Date;
+
+    /**
+     * Get the value of Date
+     *
+     * @return the value of Date
+     */
+    public String getDate() {
+        return Date;
+    }
+
+    /**
+     * Set the value of Date
+     *
+     * @param Date new value of Date
+     */
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+
+}
---
+++ src/main/java/com/mycompany/championchip/Team.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.championchip;
+
+/**
+ *
+ * @author User
+ */
+public class Team {
+    
+    private String Name;
+
+    /**
+     * Get the value of Name
+     *
+     * @return the value of Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     * Set the value of Name
+     *
+     * @param Name new value of Name
+     */
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    private String Color;
+
+    /**
+     * Get the value of Color
+     *
+     * @return the value of Color
+     */
+    public String getColor() {
+        return Color;
+    }
+
+    /**
+     * Set the value of Color
+     *
+     * @param Color new value of Color
+     */
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    private Player player1;
+
+    /**
+     * Get the value of player1
+     *
+     * @return the value of player1
+     */
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    /**
+     * Set the value of player1
+     *
+     * @param player1 new value of player1
+     */
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+}
